8fef74f07a0c34da69c88c08af9deb6d778114d8
Sage Montierth <Sissy2004M@gmail.com>
2024-04-07 18:18:37 -0700

    Sprint2

--- src/application/healthSoftware/views/MessageScreen.java
+++ src/application/healthSoftware/views/MessageScreen.java
@@ -4,32 +4,45 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.*;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+import application.healthSoftware.DataController;
 import application.healthSoftware.ScreenController;
+import application.healthSoftware.data.MessageThread;
+import application.healthSoftware.data.User;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.util.Callback;
 import javafx.scene.input.MouseEvent;
 
 public class MessageScreen implements IScreen {
 	public static String ScreenID = "messageScreen";
 	
 	ScreenController screenController;
+	DataController dataController;
+	User user = new User();
+	String threadID = new String();
+	String convertMessage;
+	Group messageThreadGroup = new Group();
+	Text messageText;
 	
-	//String to collect provider, subject and message from text fields and text areas
-	String pastMessages = new String();
-	//Placeholder label to contain providers and subjects for the inbox
-	Label inbox = new Label("No Messages");
 	
 	public MessageScreen(ScreenController sc) {
 		screenController = sc;
@@ -41,6 +54,13 @@ public class MessageScreen implements IScreen {
 	
 	public Region getLayout() {
 		
+		TextArea messageChain = new TextArea();
+		ScrollPane messageThread = new ScrollPane(messageChain);
+		 messageThread.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
+		 messageThread.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+		 VBox messageThreadBox = new VBox();
+		
+		
 		//All the stuff for the Compose message VBox
 		TextField providerText = new TextField();
 		TextField subjectText = new TextField();
@@ -51,7 +71,7 @@ public class MessageScreen implements IScreen {
 		Label provider = new Label("Provider");
 		Label subject = new Label("Subject");
 		Label inboxLbl = new Label("Inbox");
-		Button sendMess = new Button();
+		Button sendMessageButton = new Button();
 		Button backButton = new Button();
 		backButton.setText("Back");
 		backButton.setOnMouseClicked((e) -> {
@@ -67,11 +87,15 @@ public class MessageScreen implements IScreen {
 		TextArea readMessageText = new TextArea();
 		readMessageText.setMaxWidth(400);
 		readMessageText.setEditable(false);
+		Button resolve = new Button();
+		resolve.setText("Resolve");
 		
 		//Creates a list view and array to pull up message information when a message is clicked on
-		ObservableList<String> providerSubject = FXCollections.observableArrayList();
-		ListView<String> inboxMess = new ListView<String>(providerSubject);
-		ArrayList<String> individualMess = new ArrayList<String>();
+		List<MessageThread> mess =  dataController.getAllMessageThreads();
+		ObservableList<MessageThread> providerSubject = FXCollections.observableArrayList(mess);
+		ListView<MessageThread> inboxMessageThreads = new ListView<MessageThread>(providerSubject);
+		
+		
 		
 		//An alert that will pop up if any text boxes 
 		Alert emptyTextboxes = new Alert(AlertType.NONE,"Please fill out missing information",ButtonType.OK);
@@ -85,7 +109,7 @@ public class MessageScreen implements IScreen {
 		//HBox for back and send message
 		HBox bsm = new HBox();
 		//bsm.setSpacing(10);
-		bsm.getChildren().addAll(backButton, sendMess);
+		bsm.getChildren().addAll(backButton, sendMessageButton);
 		
 		//VBox for the read message part of the messaging page
 		VBox readMessage = new VBox();
@@ -93,7 +117,7 @@ public class MessageScreen implements IScreen {
 		
 		//VBox for the inbox part of the messaging page
 		VBox inboxBox = new VBox();
-		inboxBox.getChildren().addAll(inboxLbl, searchMess, inboxMess);
+		inboxBox.getChildren().addAll(inboxLbl, searchMess, inboxMessageThreads);
 		
 		//HBox for Provider label and TextField 
 		HBox providerBox = new HBox();
@@ -112,37 +136,55 @@ public class MessageScreen implements IScreen {
 		page.getChildren().addAll(inboxBox, sendMessage);
 		
 		//Event for Send Message button where text from text fields are put in the inbox label and then cleared
-		sendMess.setText("Send Message");
-		sendMess.setOnAction(new EventHandler<>() {
+		sendMessageButton.setText("Send Message");
+		sendMessageButton.setOnAction(new EventHandler<>() {
 			public void handle(ActionEvent event) {
-				//Makes sure all text boxes are filled out and if not will not proceed to the next screen
-				if(!providerText.getText().isEmpty() & !subjectText.getText().isEmpty() & !sendMessageText.getText().isEmpty() ) {
-				providerSubject.add(providerText.getText() + "\n" + subjectText.getText());
-				individualMess.add(sendMessageText.getText());
-				providerText.clear();
-				subjectText.clear();
-				sendMessageText.clear();
+				MessageThread thread = new MessageThread();
+				thread.createMessage(sendMessageText.getText(), user);
+				dataController.saveMessageThread(thread);
+			}
+		});
+		
+		inboxMessageThreads.setCellFactory(new Callback<ListView<MessageThread>, ListCell<MessageThread>>(){
+			@Override
+			public ListCell<MessageThread> call(ListView<MessageThread> m){
+				ListCell<MessageThread> message = new ListCell<MessageThread>() {
+				@Override
+				protected void updateItem(MessageThread currThread, boolean bool) {
+					super.updateItem(currThread, bool);
+					if(currThread != null) {
+						setText(String.valueOf(dataController.getAllMessageThreads()));
+					}
 				}
-				//Shows an alert that tells user to put in any missing info
-				else {
-					emptyTextboxes.show();
-				}
+				};
+				return message;
 			}
 		});
 		
 		//Event for inbox label where send message VBox is deleted and read message VBox is added
-		inboxMess.setOnMouseClicked(new EventHandler<MouseEvent>() {
+		inboxMessageThreads.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent e) {
-            	provSubjLabel.setText(inboxMess.getSelectionModel().getSelectedItem());
-            	readMessageText.setText(individualMess.get(inboxMess.getSelectionModel().getSelectedIndex()));
+            	//convertMessage = String.valueOf(inboxMessageThreads.getSelectionModel().getSelectedItem());
+            	//messageThreadGroup.getChildren().addAll(dataController.getMessageThread(threadID));
+            	//messageThread.setText(dataController.getMessageThread(convertMessage));
+            	//inboxMessageThreads.getSelectionModel().getSelectedItem()
             	page.getChildren().remove(sendMessage);
             	page.getChildren().add(readMessage);
             }
+            //dataController.getMessageThread(
           });
 		
 		//Event for back label where read message VBox is deleted and send message VBox is added
 		back.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent e) {
+               page.getChildren().remove(readMessage);
+               page.getChildren().add(sendMessage);
+            }
+          });
+		
+		resolve.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent e) {
                page.getChildren().remove(readMessage);
@@ -160,7 +202,16 @@ public class MessageScreen implements IScreen {
 		//Actually send messages to people
 		//Figure out how to handle long text
 		//Reply and forwarding functions
-		//
+		
+		//getAllMessageThreadsForPatient specifically checks the authorID field on each thread
+		
+		/*As a user, I want to be able to create threads and messages, so I can communicate with other users.
+		Acceptance Criteria:
+		Users of any kind can create a new MessageThread, which will be listed in the sidebar of the message screen.
+		Users can open a MessageThread, which will then list all messages sent in that thread, who sent them, and when they were sent.
+		Users can open a MessageThread, and send their own message.
+		Users can mark a MessageThread as resolved, removing it from the list on the left.
+*/
 	}
 	
 }
